96c7f3da19d7754aba6c5019e9b6b716438df1b8
d2004 <davideramondetti@yahoo.it>
2015-07-13 21:08:19 +0200

    Verify if the surveys will be completed

--- check.php
+++ check.php
@@ -1,1 +1,13 @@
-<?php 
+<?php
+session_start();
+require_once('config.php');
+if ($_COOKIE['surveyok'] and $_SESSION['surveyok']){
+  echo "The survey will be completed. Please wait the redirecting.. ";
+  setcookie('surveyok',null);
+  if (unset($_SESSION['surveyok']) && !$_COOKIE['surveyok']){
+    header('refresh: 5; URL='.$survey_continue);
+  } else {
+    echo "UNSETTING ERROR (400x) - Please retry again";
+  }
+}
+?>
