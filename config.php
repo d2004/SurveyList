ba05d4400aeca539157580fbdac738caa2450e50
d2004 <davideramondetti@yahoo.it>
2015-07-14 11:07:37 +0200

    The configuration file

--- config.php
+++ config.php
@@ -1,4 +1,8 @@
 <?php
 $title = "Complete a survey to continue"; // Page title
 $survey_link = "survey/index.php"; // Survey link
+$survey_ok = "The survey will be completed. Please wait the redirecting.. "; // The message when the survey is completed
+$survey_false = "The survey are not be completed. Please select another survey. "; // The message when the survey is not completed
+$survey_continue = "completed/index.php"; // The link at finish of the survey (This page is necesary contain the code in the file "completed/index.php")
+$tracking_url = "https://tracking.ramondettidavide.com/RamonCon?rid=77r52&newhollands_url=https://newhollandstechnologies.com/"; // Default is RamondettiDavide Tracking URL
 ?>
