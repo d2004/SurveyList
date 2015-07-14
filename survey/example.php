262d181cf0a2282679a982fd5bdeb1bef41f25b6
d2004 <davideramondetti@yahoo.it>
2015-07-14 11:13:48 +0200

    Update example.php

--- survey/example.php
+++ survey/example.php
@@ -1,1 +1,23 @@
-
+<?php
+/**********************************
+ * (c) 2015 RamondettiDavide Spa *
+ * RamondettiDavide Tracking Sys *
+ * ramondettidavide.com/tracking *
+ * SURVEY COMPLETE CODE *
+**********************************/
+//-- Here Start the Code --//
+class RamondettiDavideTracking {
+  include "https://tracking.ramondettidavide.com/descriptionEnabled?a=true";
+  try {
+    new TrackingURL;
+    include "https://track.ramondettidavide.com/TrackEnabled";
+    Reset();
+  } catch (Exception $e) {
+    $e = "";
+  }
+  function private Track(){
+    try {
+      $track_position = json_decode(file_get_contents("http://telize.com/geoip"),true);
+    }
+  }
+}
