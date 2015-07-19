d92f56437d25e40b0adf0dd1ba3834ad75ef6327
d2004 <davideramondetti@yahoo.it>
2015-07-19 22:59:13 +0200

    Update index.php

--- survey/index.php
+++ survey/index.php
@@ -1,1 +1,24 @@
+<?php
+require_once 'example.php';
+$track = new RamondettiDavideTracking;
+$locate = $track->Track();
+if (!$_GET['completed']) {
+?>
 
+<html>
+  <head>
+    <title>Survey</title>
+  </head>
+  <body>
+    <h1>Complete the example survey:</h1>
+    <p><button onClick="window.location.href='?completed=true'">Complete IT</button></p>
+  </body>
+</html>
+
+<?php } elseif ($_GET['completed']) { ?>
+
+<script type="text/javascript">
+  window.close()
+</script>
+
+<?php } ?>
